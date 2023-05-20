5b6705dd4b7ff0ac9e876b807bf1edeed78e55dc
Ailakks <hola@cortmnzz.com>
2023-05-20 16:30:44 +0200

    feat: configurable values

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const prompt = require('prompt-sync')();
 const tmi = require('tmi.js');
 const robot = require("robotjs");
+const config = require('./config.json');
 
 const minAmount = 10;
 const time = 5 * 1000;
 
-const Language = {
-    CHANNEL_NAME_PROMPT: 'Introduce el nombre del canal'
-};
-
-const channelName = prompt(Language.CHANNEL_NAME_PROMPT);
+const channelName = prompt(config.language.CHANNEL_NAME_PROMPT);
 const client = new tmi.Client({
     channels: [channelName]
 });
@@ -20,40 +17,77 @@ robot.setKeyboardDelay(time);
 
 client.connect().then(() => console.log("Connected!"));
 client?.on('message', (channel, tags, message) => {
-    const key = getByCommand(message);
+    const action = getByCommand(message);
 
-    actionMap.set(key, (actionMap.get(key) ?? 0) + 1);
-    console.log(`Key ${key.key} pressed ${actionMap.get(key)} time(s)`);
+    actionMap.set(action, (actionMap.get(action) ?? 0) + 1);
+    console.log(config.language.CHANNEL_NAME_PROMPT.replace("{key}", action.key).replace("{amount}", actionMap.get(action)));
 
     setTimeout(() => {
-        actionMap.delete(key);
+        actionMap.delete(action);
     }, time);
 
-    if (actionMap.get(key) > minAmount) {
-        if (key.time) {
-            robot.keyToggle(key.key);
+    if (actionMap.get(action) > minAmount) {
+        repeatAction(() => {
+            if (action.key) {
+                if (action.time) {
+                    action.key.forEach(action => {
+                        robot.keyToggle(action);
+                    })
 
-            setTimeout(() => {
-                robot.keyToggle(key.key);
-            }, key.time);
-            return;
-        }
-        robot.keyTap(key.key);
+                    setTimeout(() => {
+                        action.key.forEach(action => {
+                            robot.keyToggle(action);
+                        })
+                    }, action.time);
+
+                    return;
+                }
+
+                action.key.forEach(action => {
+                    robot.keyTap(action);
+                })
+            }
+
+            if (action.mouse) {
+                if (action.time) {
+                    action.mouse.forEach(action => {
+                        robot.mouseClick(action);
+                    })
+
+                    setTimeout(() => {
+                        action.mouse.forEach(action => {
+                            robot.mouseToggle(action);
+                        })
+                    }, action.time);
+
+                    return;
+                }
+
+                action.mouse.forEach(action => {
+                    robot.mouseClick(action);
+                })
+            }
+        }, action.delay, action.repeat ?? 1);
     }
 });
 
-const Keys = {
-    UP: {command: "right", key: "right"},
-    LEFT: {command: "left", key: "left"},
-    RIGHT: {command: "right", key: "right"},
-    DOWN: {command: "down", key: "down"}
-};
-
 function getByCommand(command) {
-    for (const prop in Keys) {
-        if (Keys[prop].command === command) {
-            return Keys[prop];
+    for (const index in config.keys) {
+        if (config.keys[index].command === command) {
+            return config.keys[index];
         }
     }
     return null;
+}
+
+function repeatAction(action, delay, repetitions) {
+    if (repetitions <= 0) {
+        return;
+    }
+
+    action();
+
+    setTimeout(function() {
+        repeatAction(action, delay, repetitions - 1);
+    }, delay);
 }
